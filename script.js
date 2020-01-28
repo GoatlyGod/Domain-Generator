8d630b8b5517e6beb05075d6721f6950bfefe9a0
NCONTED <NCONTED@mynorth.mymdc.net>
2020-01-28 11:44:06 -0500

    COMPLETE

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const pronoun = ['the','our']; 
+    const adj = ['great', 'big' ]; 
+    const noun = ['jogger','racoon'];
+
+    for(let i= 0; i< pronoun.length; i++){
+
+        for(let j=0; j< adj.length; j++){
+
+            for(let k=0; k< noun.length; k++){
+                this.console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`)
+            }
+        }   
+    }  
+}
